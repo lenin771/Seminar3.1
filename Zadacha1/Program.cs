d3a965ed1cc29ce61193c680efe737555871014c
Your Name <you@example.com>
2022-08-30 23:58:07 +0300

    Первая задача готова

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите превое число");
+string input1 = Console.ReadLine ();
+Console.WriteLine ("Введите второе число");
+string input2 = Console.ReadLine ();
+int firstNumber = int.Parse (input1);
+int secondNumber = int.Parse (input2);
+if (firstNumber > secondNumber)
+    Console.WriteLine ($"Число {firstNumber} является большим, а число {secondNumber} является меньшим");
+else
+    Console.WriteLine ($"Число {secondNumber} явялется большим, а число {firstNumber} является меньшим");
+
