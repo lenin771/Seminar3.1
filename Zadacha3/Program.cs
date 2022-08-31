f200d0dfa58be68885fea48bee2c57f7798907a3
Your Name <you@example.com>
2022-08-31 18:46:51 +0300

    Добавили задачу 3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите число");
+string input = Console.ReadLine ();
+int Number = int.Parse (input);
+if ( Number % 2 == 0)
+    {
+    Console.WriteLine ($"Число {Number} является четным");
+    }
+else 
+Console.WriteLine ($"Число {Number} является нечетным");
