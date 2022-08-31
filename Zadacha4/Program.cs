c23884eef5a5803061ae2a57b1e40a6b4ee88812
Your Name <you@example.com>
2022-08-31 19:14:28 +0300

    Добавил четвертую задачу

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите число");
+string input = Console.ReadLine ();
+int Number = int.Parse (input);
+for ( int i = 1; i <= Number; i++)
+    {
+        if (i % 2 == 0)
+        Console.WriteLine (i);
+    }
