e86d5be0d5ac48cd995eb5817e073c18c375ff14
Your Name <you@example.com>
2022-08-31 10:46:51 +0300

    Добавили задачу 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите первое число");
+string input1 = Console.ReadLine ();
+Console.WriteLine ("Введите второе число");
+string input2 = Console.ReadLine ();
+Console.WriteLine ("Введите третье число");
+string input3 = Console.ReadLine ();
+int Number1 = int.Parse (input1);
+int Number2 = int.Parse (input2);
+int Number3 = int.Parse (input3);
+if (( Number1 > Number2) && (Number1 > Number3 ))
+    {
+    Console.WriteLine ($"Число {Number1} является большим");
+    }
+else if (( Number2 > Number1) && (Number2 > Number3 ))
+    {
+    Console.WriteLine ($"Число {Number2} является большим");
+    }
+else 
+Console.WriteLine ($"Число {Number3} является большим");
